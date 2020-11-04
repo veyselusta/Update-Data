0dd37804533b4242a3312aa990bf9cebed605699
Your Name <you@example.com>
2020-11-04 18:07:49 +0300

    First project files were created

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import ScotchInfoBar from "./ScotchInfoBar";
+
+function App() {
+  // MOST OF YOUR CODE GOES HERE
+
+  return (
+    <div className="App">
+      <div>
+        <h2 className="subtitle is-4">Update Data from an input</h2>
+      </div>
+
+      {/* Display Data */}
+      <div className="input-display">
+        <p>Display Name: </p>
+        <p>Display Age: </p>
+      </div>
+
+      {/* Collect User Inputs */}
+      <div className="inputs">
+        {/* Input name */}
+        <div className="field">
+          <label className="label">Name: </label>
+          <input className="input" type="text" placeholder="William" />
+        </div>
+
+        {/* Input age */}
+        <div className="field">
+          <label className="label">Age: </label>
+          <input className="input" type="number" placeholder="38" />
+        </div>
+      </div>
+      <ScotchInfoBar seriesNumber="3" />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
